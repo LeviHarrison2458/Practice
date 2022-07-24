3dd95f4822dfb6c077505611a5ad296a562aa8fe
leviharrison2458 <leviharrison237@gmail.com>
2022-07-23 23:41:29 -0700

    Phew!

---
+++ Hello World/src/Practice.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+    public class Practice {
+        public static void main(String[] args){
+        	Scanner userInput = new Scanner(System.in);
+            System.out.println("Who are you? ");
+            String name = userInput.nextLine();
+        	int x;
+        	int u;
+        	int q;
+        	int p;
+        	int $;
+        	
+        
+        	
+       	 	for (int i=0; i<15;i++)
+       	 	System.out.println(i);
+        		 System.out.printf(" Hello %s",  name);
+        	
+        	}
+        }
